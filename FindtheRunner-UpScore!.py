a9c549f9650c1a53008a31553c905467db7160f2
kayabozbell <104430544+kayabozbell@users.noreply.github.com>
2022-06-15 01:37:05 +0300

    Update FindtheRunner-UpScore!.py

--- FindtheRunner-UpScore!.py
+++ FindtheRunner-UpScore!.py
@@ -2,13 +2,13 @@
 #İkinci en yüksek alan yarışmacıyı bulan kod satırını yazınız.
 n = int(input())
 arr = map(int, input().split())
-a=list(arr)
-maxnum=max(a)
-order=0
-runnerup=0
-while order<n :
-    if a[order]>runnerup and a[order] != maxnum:
-        runnerup=a[order]
+a = list(arr)
+maxnum = max(a)
+order = 0
+runnerup = 0
+while order < n:
+    if a[order] > runnerup and a[order] != maxnum:
+        runnerup = a[order]
     order += 1
 print(runnerup)
 print(maxnum)
